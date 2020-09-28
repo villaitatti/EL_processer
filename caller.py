a8ea204c2e951a94fd47f7df115a63042c6c12be
gspinaci <gianmarco.spinaci@gmail.com>
2020-09-28 10:23:13 +0200

    rdf compliant to rs dpub

--- caller.py
+++ caller.py
@@ -2,7 +2,10 @@ import requests
 import os
 import json
 import ast
+import uuid
 from bs4 import BeautifulSoup
+from rdflib import Graph, URIRef, namespace, Namespace, Literal
+from datetime import datetime
 
 class End2End: 
   
@@ -82,7 +85,7 @@ class End2End:
                     if s[index:index+len(char)] == char:
                         return index
 
-            index += 1
+                index += 1
 
         return -1
 
@@ -98,7 +101,9 @@ class End2End:
 
             self.body = f'{self.body[:start]}<annotation text="{text}" end_offset="{l}" wikidata_id="{wikidata_id}">{text}</annotation>{self.body[end:]}'
 
-    def get_rdf_annotation(self):
+    def write_rdf_annotation(self):
+
+        filename = self.name.replace('.html','')
 
         self.update_body()
 
@@ -107,19 +112,113 @@ class End2End:
 
         for annotation in self.soup.findAll('annotation'):
 
-            self.annotations_json.append({
-                'text': annotation['text'],
-                'wikidata_id': annotation['wikidata_id'],
-                key_start: {
-                    key_xpath_selector: self.xpath_soup(annotation.parent),
-                    key_text_position_selector: {
-                        key_start: self.find_str(annotation.parent.text, annotation['text']),
-                        key_end: self.find_str(annotation.parent.text, annotation['text']) + int(annotation['end_offset'])
-                    }
-                }
-            })
+            base_uri = 'https://collection.itatti.harvard.edu/yashiro/annotation/'
+            wikidata_base_uri = 'https://www.wikidata.org/wiki/'
 
-        return self.annotations_json
+            xpath = self.xpath_soup(annotation.parent)
+            offset =  self.find_str(annotation.parent.text, annotation['text'])
+
+            g = Graph()
+
+            # namespaces
+            RDF = namespace.RDF
+            XSD = namespace.XSD
+            RDFS = namespace.RDFS
+            OA = Namespace('http://www.w3.org/ns/oa#')
+            CRMDIG = Namespace('http://www.ics.forth.gr/isl/CRMdig/')
+            CRM = Namespace('http://www.cidoc-crm.org/cidoc-crm/')
+            USER = Namespace('http://www.metaphacts.com/resource/user/')
+            PLATFORM = Namespace('http://www.metaphacts.com/ontologies/platform#')
+            LDP = Namespace('http://www.w3.org/ns/ldp#')
+            PROV = Namespace('http://www.w3.org/ns/prov#')
+
+            # URIs            https://collection.itatti.harvard.edu/resource/yashiro-document:Letter_036
+            base_uri_file = f'https://collection.itatti.harvard.edu/resource/yashiro/document/{filename}'
+            
+            id = str(uuid.uuid1())
+            base_node_uri = f'{base_uri}{id}'
+            base_node_body_uri = f'{base_node_uri}/body'
+            base_node_event_uri = f'{base_uri_file}/annotation-event-{uuid.uuid1()}'
+            base_node_event_date_uri = f'{base_node_event_uri}/modifiedAt'
+
+            range_source_uri = f'{base_uri_file}/range-source-{uuid.uuid1()}'
+            range_uri = f'{base_uri_file}/range-{uuid.uuid1()}'
+            start_selector_uri = f'{base_uri_file}/xpath-{uuid.uuid1()}'
+            end_selector_uri = f'{base_uri_file}/xpath-{uuid.uuid1()}'
+            
+            start_offset_uri = f'{base_uri_file}/offset-{uuid.uuid1()}'
+            end_offset_uri = f'{base_uri_file}/offset-{uuid.uuid1()}'
+
+            wikidata_object_uri = f'{wikidata_base_uri}{annotation["wikidata_id"]}'
+
+            BASE_NODE_CONTAINER = URIRef(f'{base_node_uri}/container')
+
+            BASE_NODE = URIRef(base_node_uri)
+            BASE_NODE_BODY = URIRef(base_node_body_uri)
+            BASE_NODE_EVENT = URIRef(base_node_event_uri)
+            BASE_NODE_EVENT_DATE = URIRef(base_node_event_date_uri)
+            RANGE_SOURCE = URIRef(range_source_uri)
+            RANGE = URIRef(range_uri)
+
+            START_SELECTOR = URIRef(start_selector_uri)
+            START_OFFSET = URIRef(start_offset_uri)
+
+            END_SELECTOR = URIRef(end_selector_uri)
+            END_OFFSET = URIRef(end_offset_uri)
+
+            LETTER = URIRef(base_uri_file)
+            WIKIDATA_OBJECT = URIRef(wikidata_object_uri)
+
+            g.add( (PLATFORM.formContainer, LDP.contains, BASE_NODE_CONTAINER) )
+
+            g.add( (BASE_NODE_CONTAINER, RDF.type, LDP.Resource) )
+            g.add( (BASE_NODE_CONTAINER, RDF.type, PROV.Entity) )
+            g.add( (BASE_NODE_CONTAINER, PROV.generatedAtTime, Literal(datetime.now().strftime(timestring))) )
+            g.add( (BASE_NODE_CONTAINER, PROV.wasAttributedTo, USER.EL_processer) )
+
+
+            g.add( (BASE_NODE, RDF.type, CRMDIG.D29_Annotation_Object) )
+            g.add( (BASE_NODE, RDF.type, OA.Annotation) )
+            g.add( (BASE_NODE, CRMDIG.L48i_was_annotation_created_by, BASE_NODE_EVENT) )
+            g.add( (BASE_NODE, OA.hasBody, BASE_NODE_BODY) )
+            g.add( (BASE_NODE, OA.hasTarget, RANGE_SOURCE) )
+
+            g.add( (BASE_NODE_BODY, CRM.P1_is_identified_by, WIKIDATA_OBJECT) )
+            g.add( (BASE_NODE_BODY, RDF.type, CRM.E1_Entity) )
+
+            g.add( (BASE_NODE_EVENT, RDF.type, CRMDIG.D30_Annotation_Event) )
+            g.add( (BASE_NODE_EVENT, CRM.P14_carried_out_by, USER.EL_processer) )
+            g.add( (BASE_NODE_EVENT, CRM.P4_has_time_span, BASE_NODE_EVENT_DATE) )
+
+            g.add( (BASE_NODE_EVENT_DATE, CRM.P81a_end_of_the_begin, Literal(datetime.now().strftime(timestring))) )
+            g.add( (BASE_NODE_EVENT_DATE, CRM.P81b_begin_of_the_end, Literal(datetime.now().strftime(timestring))) )
+
+            g.add( (RANGE_SOURCE, RDF.type, OA.SpecificResource) )
+            g.add( (RANGE_SOURCE, RDF.value, Literal(annotation[key_text])) )
+            g.add( (RANGE_SOURCE, OA.hasSource, LETTER) )
+            g.add( (RANGE_SOURCE, OA.hasSelector, RANGE) )
+
+            g.add( (RANGE, RDF.type, OA.RangeSelector) )
+            g.add( (RANGE, OA.hasStartSelector, START_SELECTOR) )
+            g.add( (RANGE, OA.hasEndSelector, END_SELECTOR) )
+
+            g.add( (START_SELECTOR, RDF.type, OA.XPathSelector) )
+            g.add( (START_SELECTOR, RDF.value, Literal(xpath)) )
+            g.add( (START_SELECTOR, OA.refinedBy, START_OFFSET) )
+
+            g.add( (START_OFFSET, RDF.type, OA.TextPositionSelector) )
+            g.add( (START_OFFSET, OA.start, Literal(offset, datatype=XSD.nonNegativeInteger)) )
+            g.add( (START_OFFSET, OA.end, Literal(offset, datatype=XSD.nonNegativeInteger)) )
+
+            g.add( (END_SELECTOR, RDF.type, OA.XPathSelector) )
+            g.add( (END_SELECTOR, RDF.value, Literal(xpath)) )
+            g.add( (END_SELECTOR, OA.refinedBy, END_OFFSET) )
+
+            g.add( (END_OFFSET, RDF.type, OA.TextPositionSelector) )
+            g.add( (END_OFFSET, OA.start, Literal(offset + int(annotation['end_offset']), datatype=XSD.nonNegativeInteger)) )
+            g.add( (END_OFFSET, OA.end, Literal(offset + int(annotation['end_offset']), datatype=XSD.nonNegativeInteger)) ) 
+
+            g.serialize(destination=f'{os.path.join(dir_path, dir_data, dir_rdf, id)}.ttl', format='turtle')
 
 class Adapter: 
     """ 
@@ -144,6 +243,8 @@ class Adapter:
 
 """ main method """
 if __name__ == "__main__": 
+
+    timestring = '%Y-%m-%dT%H:%M:%S'
 
     key_text = 'text'
     key_spans = 'spans'
@@ -182,4 +283,4 @@ if __name__ == "__main__":
                 objects.append(Adapter(End2End, annotations = End2End.get_annotations)) 
 
     for obj in objects: 
-        print(obj.get_rdf_annotation())
+        obj.write_rdf_annotation()
